7cfc21e5114f7e3a3c8abc4a732d2425df121006
phucpt <phucpt@moolapp.com>
2021-12-29 00:24:34 +0700

    create project

---
+++ app/src/main/java/com/example/a12cunghdtechjak13/view/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.a12cunghdtechjak13.view.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.a12cunghdtechjak13.R;
+
+/**
+ * Created by phucpt on 12/28/2021
+ */
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actitvity_splash_screen);
+    }
+}
